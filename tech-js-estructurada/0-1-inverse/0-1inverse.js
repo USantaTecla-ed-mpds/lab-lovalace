84cfe44f5b5ec493ca414476b0041a7518fe5fb9
ManuelRCastroIglesias <manuel.r.castro.iglesias@hotmail.com>
2022-05-07 07:15:46 +0200

    Correciones 1

--- tech-js-estructurada/0-1-inverse/0-1inverse.js
+++ tech-js-estructurada/0-1-inverse/0-1inverse.js
@@ -3,19 +3,22 @@ const console = new Console();
 /// <Autor: Manuel Rosendo Castro Iglesias>
 /// Most programmers read and write in the Dextroverse Latin alphabet, and so texts are "read" from left to right.
 
-const askedNumerator; let calcNumerator;
-do {!(askedNumerator < 1) ? askedNumerator = console.readNumber(`Introduce el numerador de la fracción: `) : calcNumerator = askedNumerator;
+const askedNumerator; let calcNumerator; // ¿undefined?
+do {
+    !(askedNumerator < 1) ? askedNumerator = console.readNumber(`Introduce el numerador de la fracción: `) : calcNumerator = askedNumerator;
 } while (askedNumerator > 0);
 
-const askedDenominator; let calcDenominator;
-do {(!askedDenominator < 1) ? askedNumerator = console.readNumber(`Introduce el numerador de la fracción: `) : calcDenominator = askedDenominator;
+const askedDenominator; let calcDenominator; // ¿undefined?
+do {
+    (!askedDenominator < 1) ? askedDenominator = console.readNumber(`Introduce el numerador de la fracción: `) : calcDenominator = askedDenominator;
 } while (askedNumerator > 0);
 
-const txtNumerator; const txtDenominator;
-do {(askedNumerator > askedDenominator) ? calcNumerator = calcNumerator % calcDenominator : calcDenominator = calcDenominator % calcNumerator;
+do {
+    (askedNumerator > askedDenominator) ? calcNumerator = calcNumerator % calcDenominator : calcDenominator = calcDenominator % calcNumerator;
 } while (askedNumerator !== 1 || askedDenominator !== 1);
 
-// Tengo dudas si es válido ( ... , ...);
+// Tengo dudas si es válido ( … , … );
+const txtNumerator; const txtDenominator; // ¿undefined?
 calcNumerator > calcDenominator ? (txtNumerator = calcNumerator, txtDenominator = calcDenominator) : (txtNumerator = calcDenominator, txtDenominator = calcNumerator);
 
 console.writeln(`La fracción ${askedNumerator}/${askedDenominator} = ${txtNumerator}/${txtDenominator} invertida es la fracción ${txtDenominator}/${txtNumerator}`);
