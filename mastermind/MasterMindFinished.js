59a5690202616037fc7d1a358b82ca561a8b5e5b
Alberto81 <albertoantelopresa@gmail.com>
2022-06-16 18:57:02 +0200

    Update MasterMindFinished.js

--- mastermind/MasterMindFinished.js
+++ mastermind/MasterMindFinished.js
@@ -5,112 +5,119 @@ do {
 } while (isResumed())
 
 function isResumed() {
-    let resume = console.readString(`quieres continuar? (y/n):`)
-    if (resume.charAt(0) === "y") {
-        return true;
-    } else {
-        return false;
-    }
+    let resume = console.readString(`quieres continuar? (y/n):`);
+    return resume[0] === "y";
 }
 function playGame() {
-    const COLORS = ["r", "g", "y", "b", "m", "c"]
-    const COLORSLENGTH=COLORS.length;
-    const COMBINATIONLENGTH=4;
-    const SecretCombination = changeIndexToColors(getIndexCombination(random,COMBINATIONLENGTH, COLORSLENGTH));
+    const COLORS = ["r", "g", "y", "b", "m", "c"];
+    const COMBINATION_LENGTH = 4;
+    const SECRET_COMBINATION = getSecretCombination(COMBINATION_LENGTH, COLORS);
+    console.writeln(SECRET_COMBINATION);
     const MOVES = 10;
-    let intentos = MOVES;
+    let attempts = MOVES;
     let proposedBoard = [];
-    let responseBoard = [];
     let messageOut = "";
-  
-    for (let i = 0; !found(responseBoard) && i < MOVES; i++) {
-        console.writeln(`--Adelante! ${intentos--} intentos restantes--`);
-        let proposedCombination = getProposedCombination(COLORS, COMBINATIONLENGTH);
-        console.writeln(proposedCombination);
-        const response = responseCombination(proposedCombination, SecretCombination);
-        saveInBoard(proposedBoard, proposedCombination);
-        saveInBoard(responseBoard, response);
-        printBoard(proposedBoard, responseBoard);
-        messageOut = winMessage(response);
-    }
+    let proposedCombination = [];
+    do {
+        console.writeln(`--Adelante! ${attempts--} intentos restantes--`);
+        proposedCombination = getProposedCombination(COLORS, COMBINATION_LENGTH);
+        proposedBoard[proposedBoard.length] = proposedCombination;
+        printBoard(proposedBoard, SECRET_COMBINATION);
+        messageOut = winMessage(proposedCombination, SECRET_COMBINATION);
+    } while (!found(proposedCombination, SECRET_COMBINATION) && attempts > 0)
     console.writeln(messageOut);
-    function getProposedCombination(arrayColors, COMBINATIONLENGTH) {
-        const COLORS = arrayColors;
+
+    function getSecretCombination(combinationLength, colors) {
+        let indexCombination = [];
+        let indexColor;
+        while (indexCombination.length < combinationLength) {
+            indexColor = parseInt(Math.random() * colors.length);
+            if (indexCombination.length == 0) {
+                indexCombination[indexCombination.length] = indexColor;
+            } else {
+                let repeated = false;
+                for (let i = 0; i < indexCombination.length; i++) {
+                    repeated ||= indexColor == indexCombination[i];
+                }
+                if (!repeated) {
+                    indexCombination[indexCombination.length] = indexColor;
+                }
+            }
+        }
+        let colorsArray = [];
+        for (let i = 0; i < indexCombination.length; i++) {
+            colorsArray[colorsArray.length] = COLORS[indexCombination[i]];
+        }
+        return colorsArray;
+    }
+    function getProposedCombination(arrayColors, combinationLength) {
         let error;
         let proposedArray = [];
         do {
             error = false;
-            console.writeln(`Los colores son ${COLORS}`)
-            let proposedString = console.readString(`Introduce 4 colores`)
-            error = checkLenght(proposedString, COMBINATIONLENGTH);
-            if (!error) {
-                proposedArray = changeStringToArray(proposedString,COMBINATIONLENGTH);
-            }
-            if (!error) {
-                error = checkRepeated(proposedArray);
-            }
-            if (!error) {
-                error = checkInvalidsColors(proposedArray, COLORS)
-            }
-        } while (error)
-        return proposedArray;
-        function checkInvalidsColors(proposedArray, arrayColors) {
-            let found = true;
-            const COLORS = arrayColors;
-            for (i = 0; found && i < proposedArray.length; i++) {
-                found = false;
-                for (j = 0; !found && j < COLORS.length; j++) {
-                    found = proposedArray[i] === COLORS[j];
-                }
-            }
-            if (!found) {
-                console.writeln(`Error, color desconocido.`);
-            }
-            return !found;
-        }
-        function checkRepeated(proposedArray) {
-            let repeated = false;
-            for (i = 0; !repeated && i < proposedArray.length; i++) {
-                for (j = i + 1; !repeated && j <= proposedArray.length; j++) {
-                    repeated = proposedArray[i] === proposedArray[j];
-                }
-            }
-            if (repeated) {
-                console.writeln(`Error, colores repetidos.`);
-            }
-            return repeated;
-        }
-        function changeStringToArray(string, COMBINATIONLENGTH) {
-            let proposedArray = [];
-            for (i = 0; i < COMBINATIONLENGTH; i++) {
-                proposedArray[i] = string.charAt(i);
-            }
-            return proposedArray;
-        }
-        function checkLenght(string, COMBINATIONLENGTH) {
-            let error = string.length != COMBINATIONLENGTH;
+            console.writeln(`Los colores son ${arrayColors}`);
+            let proposedString = console.readString(`Introduce ${combinationLength} colores`);
+            error = proposedString.length != combinationLength;
             if (error) {
                 console.writeln(`Error, cantidad de colores incorrecta.`);
             }
-            return error;
+            if (!error) {
+                error = checkRepeated(proposedString);
+                if (error) {
+                    console.writeln(`Error, colores repetidos.`);
+                }
+            }
+            if (!error) {
+                let validColor = true;
+                for (let i = 0; validColor && i < combinationLength; i++) {
+                    validColor = checkValidsColors(proposedString[i], arrayColors);
+                }
+                if (!validColor) {
+                    console.writeln(`Error, color desconocido.`);
+                }
+                error = !validColor;
+            }
+            if (!error) {
+                proposedArray = changeStringToArray(proposedString, combinationLength);
+            }
+        } while (error)
+        return proposedArray;
+
+        function checkValidsColors(color, arrayColors) {
+            let colorFound = false;
+            for (let i = 0; !colorFound && i < arrayColors.length; i++) {
+                colorFound = color === arrayColors[i];
+            }
+            return colorFound;
+        }
+        function checkRepeated(proposedString) {
+            let repeated = false;
+            for (let i = 0; !repeated && i < proposedString.length-1; i++) {
+                for (let j = i + 1; !repeated && j < proposedString.length; j++) {
+                    repeated = proposedString[i] === proposedString[j];
+                }
+            }
+            return repeated;
+        }
+        function changeStringToArray(string) {
+            let proposedArray = [];
+            for (let i = 0; i < string.length; i++) {
+                proposedArray[i] = string[i];
+            }
+            return proposedArray;
         }
     }
-    function found(board) {
+    function found(proposed, secret) {
         let result = true;
-        if (board.length == 0) {
-            return false;
-        } else {
-            for (let item of board[board.length - 1]) {
-                result &&= item === "Negro";
-            }
+        for (let i in proposed) {
+            result &&= proposed[i] === secret[i];
         }
         return result;
     }
-    function winMessage(lastResponse) {
-        let response = lastResponse;
+    function winMessage(proposed, secret) {
         let result = true;
-        for (let item of response) {
-            result &&= item === "Negro";
+        for (let i in proposed) {
+            result &&= proposed[i] === secret[i];
         }
         if (result) {
             return `has ganado!`;
@@ -118,76 +125,38 @@ function playGame() {
             return `has perdido, se acabaron los intentos`;
         }
     }
-    function printBoard(proposedB, responseB) {
+    function printBoard(proposedBoard, secret) {
         let message = "\n***\n";
-        for (i = 0; i < proposedB.length; i++) {
-            message += `${proposedB[i][0]}${proposedB[i][1]}${proposedB[i][2]}${proposedB[i][3]} -->  ${writeResponseToString(responseB[i])}\n`
+        for (let proposed of proposedBoard) {
+            message += `${writeProposed(proposed)} --> ${writeResponse(proposed, secret)}\n`;
         }
         console.writeln(message);
-        function writeResponseToString(response) {
-            let array = response;
-            let countWhite = 0;
-            let countBlack = 0;
-            for (let color of array) {
-                if (color === "Blanco") {
-                    countWhite++;
-                } else if (color === "Negro") {
-                    countBlack++;
+        function writeProposed(proposed) {
+            let proposedLine = "";
+            for (let color of proposed) {
+                proposedLine += color;
+            }
+            return proposedLine;
+        }
+        function writeResponse(proposed, secret) {
+            let whitesAndBlacks = 0;
+            for (let proposedColor of proposed) {
+                let sameColor = false;
+                for (let secretColor of secret) {
+                    sameColor ||= proposedColor === secretColor;
+                }
+                if (sameColor) {
+                    whitesAndBlacks++;
                 }
             }
-            return `${countBlack} negros y ${countWhite} blancos`;
+            let blacks = 0;
+            for (let i in secret) {
+                if (secret[i] === proposed[i]) {
+                    blacks++;
+                }
+            }
+            let whites = whitesAndBlacks - blacks
+            return `${blacks} negros y ${whites} blancos`
         }
-    }
-    function saveInBoard(board, combination) {
-        board[board.length] = combination;
-    }
-    function responseCombination(proposed, secret) {
-        const RESPONSECOLORS = ["Nada", "Blanco", "Negro"];
-        let response = [RESPONSECOLORS[0], RESPONSECOLORS[0], RESPONSECOLORS[0], RESPONSECOLORS[0]]
-        for (i in proposed) {
-            let sameColor = false;
-            for (secretItem of secret) {
-                sameColor ||= proposed[i] === secretItem;
-            }
-            if (sameColor) {
-                response[i] = RESPONSECOLORS[1];
-            }
-        }
-        for (j in secret) {
-            if (secret[j] === proposed[j]) {
-                response[j] = RESPONSECOLORS[2];
-            }
-        }
-        return response;
-    }
-    function getIndexCombination(f, COMBINATIONLENGTH, COLORSLength) {
-        let combination = [];
-        let anIndexColor;
-        while (combination.length < COMBINATIONLENGTH) {
-            anIndexColor = f(combination.length + 1);
-            if (combination.length == 0 && anIndexColor >= 0 && anIndexColor < COLORSLength) {
-                combination[combination.length] = anIndexColor;
-            }
-            let repeated = false;
-            for (i = 0; i < combination.length; i++) {
-                repeated ||= anIndexColor == combination[i];
-            }
-            if (!repeated && anIndexColor >= 0 && anIndexColor < COLORSLength) {
-                combination[combination.length] = anIndexColor;
-            }
-        }
-        return combination;
-    }
-    function random() {
-        return parseInt(Math.random() * 6);
-    }
-    function changeIndexToColors(indexArray) {
-        const COLORS = ["r", "g", "y", "b", "m", "c"]
-        const index = indexArray;
-        let colorsArray = []
-        for (i = 0; i < index.length; i++) {
-            colorsArray[colorsArray.length] = COLORS[index[i]];
-        }
-        return colorsArray;
     }
 }
