a61a4aee46fd14147fd5ee69d465bca37ae89f58
Ignacio Cavero <ignacio.cavero@quintasenergy.com>
2023-02-03 17:00:52 +0100

    commit 2

--- tech-java-basada-en-objetos/Coordinate.java
+++ tech-java-basada-en-objetos/Coordinate.java
@@ -3,6 +3,25 @@ public class Coordinate {
     private int CoordinateX;
     private int CoordinateY;
 
+    //Constructors
     public Coordinate(){} //Default constructor that inicializate Coordinate in (0,0)
     
+    public Coordinate(int coordinateX, int coordinateY){} 
+
+    //Methods
+    public void setCoordinateX(int coordinateX) {}
+
+    public void setCoordinateY(int coordinateY) {}
+
+    public int getCoordinateX(){}
+
+    public int getCoordinateY(){}
+
+    public void move(int x, int y){}
+
+    public int distance(Coordinate coordinate){}
+
+    public void addCoordinate(Coordinate coordinate){}
+
+    public Coordinate clone(){}
 }
--- tech-java-basada-en-objetos/Date.java
+++ tech-java-basada-en-objetos/Date.java
@@ -4,7 +4,43 @@ public class Date {
     private int Month;
     private int Year;
 
+    //Constructors
     public Date(){}; //Default constructor that inicializate date to "1/1/0001"
 
+    public Date(int day, int month, int year){};
+
+    public Date(String date, String format){};
+
+
+    //Methods
+    public void setDay(int day){}
+
+    public void setMonth(int month){}
+
+    public void setYear(int year){}
+
+    public int getDay(){}
+
+    public int getMonth(){}
+
+    public int getYear(){}
+
+    public void addDays(int days){}
+
+    public void addMonths(int month){}
+
+    public void addYears(int year){}
+
+    public String dayOfTheeek(){}
+
+    public boolean isLeapYear(){}
+
+    public int difference(Date date){} //Return number of second between dates
+
+    public Date clone(){}
+
+    public boolean equals(Date date){}
+
+    public int compareDates(Date date){}
 }
 
